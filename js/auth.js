231564bf2bd84ceed73ccad8fd6ca7214243b360
Segandorin <victormmpp86@gmail.com>
2025-01-10 18:58:37 +0100

    Update auth.js

--- js/auth.js
+++ js/auth.js
@@ -1,8 +1,8 @@
 export const firebaseConfig = {
-    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-    storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-    messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-    appId: process.env.REACT_APP_FIREBASE_APP_ID
+    apiKey: "AIzaSyACWdDgSVvbUqvZbTJoWlt2bR4Mayn7ju4",
+    authDomain: "gastos-947f9.firebaseapp.com",
+    projectId: "gastos-947f9",
+    storageBucket: "gastos-947f9.firebasestorage.app",
+    messagingSenderId: "437497845278",
+    appId: "1:437497845278:web:f28f31cc2bbba9017f000f"
 };
